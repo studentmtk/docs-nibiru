73bc8f30ff17b37d5193af6a948029e18b0930f8
Unique-Divine <realuniquedivine@gmail.com>
2022-10-14 11:19:18 -0500

    feat(deploy.sh): script for publishing to pages

---
+++ docs/deploy.sh
@@ -0,0 +1,17 @@
+# Production deployment
+npm run build
+# echo nibiru.fi > dist/CNAME
+
+cd dist 
+git init 
+git add . 
+git commit -am "deploy"
+git checkout -b gh-pages
+git push -u https://github.com/NibiruChain/docs-nibiru gh-pages --force
+rm -rf .git 
+cd ..
+
+# Template: See https://stackoverflow.com/a/65733058/13305627
+# git push <remote> `git subtree split --prefix <local-folder> <local-branch>`:<remote-branch> --force
+# git push origin `git subtree split --prefix dist deploy`:gh-pages --force
+
